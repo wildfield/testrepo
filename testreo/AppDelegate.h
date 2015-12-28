05925737fdc3745036dca091cf4162a84b2874a3
Dmitry Kasianchuk <wildfield@live.ru>
2015-12-28 18:11:14 +0200

    added sample project

---
+++ testreo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testreo
+//
+//  Created by Dmitry K on 12/28/15.
+//  Copyright © 2015 Dmitry K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testreo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testreo
+//
+//  Created by Dmitry K on 12/28/15.
+//  Copyright © 2015 Dmitry K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
